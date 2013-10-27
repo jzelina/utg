03206fcc6b8960766764bea198dd22e19f43126d
jzelina <julian@onlinej.de>
2013-10-27 03:14:45 +0100

    Signed-off-by: jzelina <julian@onlinej.de>

--- terminals/generic.php
+++ terminals/generic.php
@@ -22,10 +22,16 @@ function TEXTMENU($object,$terminal,$lang)
 #
 function HTML_TEXTSCREEN($object,$terminal,$lang)
 	{
-	$out =  '<html><head><title>'.$object[0]["title"].'</title><meta http-equiv="content-type" content="text/html; charset=UTF-8" /></head>';
-	$out .= '<body>'.$object[0]["text"];
-	if ($object[0]["okurl"] != "") {$out .= '<br /><a href="?read='.$object[0]["okurl"].'">'.$lang["ok"].'</a>';}
-	if ($object[0]["cancelurl"] != "") {$out .= '<br /><a href="?read='.$object[0]["cancelurl"].'">'.$lang["cancel"].'</a>';}
+	$out =  '<html><head><title>'.$object[0]["title"].'</title><meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script></head>';
+	$out .= '<div data-role="header"><h1>'.$object[0]["title"].'</h1></div>';
+	$out .= '<body><h3>'.$object[0]["text"].'</h3>';
+	
+    if ($object[0]["okurl"] != "") {$out .= '<a href="?read='.$object[0]["okurl"].'" data-role="button" data-icon="check">'.$lang["ok"].'</a>';}
+	if ($object[0]["cancelurl"] != "") {$out .= '<a href="?read='.$object[0]["cancelurl"].'" data-role="button" data-icon="delete">'.$lang["cancel"].'</a>';}
+	
 	$out .= '</body><html>';
 	return $out;
 	}	
@@ -35,14 +41,19 @@ function HTML_TEXTSCREEN($object,$terminal,$lang)
 #
 function HTML_TEXTMENU($object,$terminal,$lang)
 	{
-	$out =  '<html><head><title>'.$object[0]["title"].'</title><meta http-equiv="content-type" content="text/html; charset=UTF-8" /></head>';
+	$out =  '<html><head><title>'.$object[0]["title"].'</title><meta http-equiv="content-type" content="text/html; charset=UTF-8" />	
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script></head>';
 	$out .= '<body>';
-	if ($object[0]["line1text"] != "") {$out .= '<br /><a href="?read='.$object[0]["line1url"].'">'.$object[0]["line1text"].'</a>';}
-	if ($object[0]["line2text"] != "") {$out .= '<br /><a href="?read='.$object[0]["line2url"].'">'.$object[0]["line2text"].'</a>';}
-	if ($object[0]["line3text"] != "") {$out .= '<br /><a href="?read='.$object[0]["line3url"].'">'.$object[0]["line3text"].'</a>';}
-	if ($object[0]["line4text"] != "") {$out .= '<br /><a href="?read='.$object[0]["line4url"].'">'.$object[0]["line4text"].'</a>';}
-	if ($object[0]["line5text"] != "") {$out .= '<br /><a href="?read='.$object[0]["line5url"].'">'.$object[0]["line5text"].'</a>';}
-	if ($object[0]["line6text"] != "") {$out .= '<br /><a href="?read='.$object[0]["line6url"].'">'.$object[0]["line6text"].'</a>';}
+	$out .= '<ul data-role="listview" data-ajax="false" data-inset="true" data-theme="d"><li data-role="list-divider">'.$object[0]["title"].'</li>';
+	if ($object[0]["line1text"] != "") {$out .= '<li><a href="?read='.$object[0]["line1url"].'">'.$object[0]["line1text"].'</a></li>';}
+	if ($object[0]["line2text"] != "") {$out .= '<li><a href="?read='.$object[0]["line2url"].'">'.$object[0]["line2text"].'</a></li>';}
+	if ($object[0]["line3text"] != "") {$out .= '<li><a href="?read='.$object[0]["line3url"].'">'.$object[0]["line3text"].'</a></li>';}
+	if ($object[0]["line4text"] != "") {$out .= '<li><a href="?read='.$object[0]["line4url"].'">'.$object[0]["line4text"].'</a></li>';}
+	if ($object[0]["line5text"] != "") {$out .= '<li><a href="?read='.$object[0]["line5url"].'">'.$object[0]["line5text"].'</a></li>';}
+	if ($object[0]["line6text"] != "") {$out .= '<li/><a href="?read='.$object[0]["line6url"].'">'.$object[0]["line6text"].'</a></li>';}
+	$out .= '</ul>';
 	$out .= '</body><html>';
 	return $out;
 	}	
